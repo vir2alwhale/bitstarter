451ca2b2c40dba6dc0a1bf95a48392471b2142e8
vir2alwhale <matej.marjanovich@gmail.com>
2013-08-15 21:17:07 +0000

    Added to send index.html

--- web.js
+++ web.js
@@ -3,6 +3,7 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
+  response.send(fs.readFileSync('index.html','String');
   response.send('Hello World2!');
 });
 
